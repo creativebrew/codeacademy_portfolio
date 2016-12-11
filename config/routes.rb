16c9f4bd9b410f608b910a509b75ec75f88de861
codecademy <ccuser@codecademy.com>
2016-12-10 21:10:26 -0500

    About

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 	root 'pages#welcome'
   get '/portfolio' => 'pages#portfolio'
+  get '/about' => 'pages#about'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
